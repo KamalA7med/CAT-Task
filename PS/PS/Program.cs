e9f7c2657e102518dc73a180c838141d9f7c1bb2
KamalA7med <kamala7med136@gmail.com>
2025-12-14 22:29:34 +0200

    Initial commit

---
+++ PS/PS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS
+{
+    internal class Program
+    {
+        public static string GetShortestString(List<string>list)
+        {
+            string str = list[0];
+            foreach(string item in list)
+            {
+                if(item.Length<str.Length)
+                {
+                    str = item;
+                }
+            }
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            List<string>list = new List<string>();
+            list.Add("Kamal");
+            list.Add("Ali");
+            list.Add("Osama");
+            list.Add("Mohamed");
+            list.Add("1");
+            string shortest_string = GetShortestString(list);
+            Console.WriteLine(shortest_string);
+        }
+    }
+}
